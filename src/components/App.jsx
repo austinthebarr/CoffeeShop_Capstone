586f767921e5c93ff4cd76446a3870d01dac741f
Austin Barr <austinbarr@protonmail.com>
2018-09-21 18:14:42 -0700

    add route for pos and check test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,16 @@ import { Switch, Route, withRouter } from 'react-router-dom';
 import Splash from './Splash';
 import AddToInventory from './AddToInventory';
 import AddToMenu from './AddToMenu';
+import Pos from './Pos';
 
 const App = () => {
   return (
     <div className='bodyStyles'> 
       <Switch>
-     
         <Route exact path='/' render={() => <Splash/>} />
         <Route path='/InventoyForm' render={() => <AddToInventory/>} />
         <Route path='/MenuForm' render={() => <AddToMenu/>} />
-      
+        <Route path='/Pos' render={() => <Pos />} />
       </Switch>
     </div>
   );
--- src/components/Pos.jsx
+++ src/components/Pos.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import Menu from './Menu';
+import CurrentSale from './CurrentSale';
+import NavBar from './NavBar';
+
 const Pos = () => {
   return(
-  <div></div>
+    <div> 
+      <div className='row col-md-12'>
+        <div className='col-md-8'>
+          <Menu />
+        </div>
+        <div className='col-md-4'>
+          <CurrentSale />
+        </div>
+      </div>
+      <div className='row col-md-12'>
+        <NavBar />
+      </div>
+    </div>
   );
 };
 
