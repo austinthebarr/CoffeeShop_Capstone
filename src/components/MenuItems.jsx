51fbea85eed67fe55df79d4a8045205276cf8266
Austin Barr <austinbarr@protonmail.com>
2018-09-21 18:04:58 -0700

    added multiple componets for pos

---
+++ src/components/MenuItems.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MenuItems = () => {
+  return(<h1>test</h1>);
+};
+
+export default MenuItems;
---
+++ src/components/Pos.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Pos = () => {
+  return(
+  <div></div>
+  );
+};
+
+export default Pos;
